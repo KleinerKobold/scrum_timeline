b631e96b58c09f308cb4a658ebc8a310149cf2b6
Oliver Staudt <oliver.r.staudt@gmail.com>
2021-09-27 22:01:21 +0200

    Added text

--- svg_writer.py
+++ svg_writer.py
@@ -2,14 +2,20 @@ import svgwrite
 from loguru import logger
 dwg = svgwrite.Drawing('svgwrite-example.svg', profile='tiny')
 
-def bonbon(x : int, y:int, width:int, dwg):
+def bonbon(x : int, y:int, width:int, title:str, dwg):
     r= 10
     dwg.add(dwg.circle((x,y),(r),fill='blue'))
     dwg.add(dwg.circle((x+width,y),(r),fill='blue'))
     dwg.add(dwg.rect((x, y-r), (width,r*2),fill='blue'))
-bonbon(20,20,100,dwg)
+    dwg.add(dwg.text(text=title,insert=(x,y+3),
+    fill='white',
+    font_size='12px',
+    font_family="Courier New"))
+    
 
-bonbon(40,50,150,dwg)
+bonbon(20,20,100,"Test",dwg)
+
+bonbon(60,60,150,"test",dwg)
 
 
 print(dwg.tostring())
