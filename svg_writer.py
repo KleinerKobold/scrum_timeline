790d4b81d59e4e3df6bb8f5e3e4c3f22e852b797
Oliver Staudt <Trollabeissa@gmail.com>
2021-09-28 20:32:06 +0200

    better if for type

--- svg_writer.py
+++ svg_writer.py
@@ -10,14 +10,19 @@ dfs = read_excel('events.xlsx')
 dwg = svgwrite.Drawing('svgwrite-example.svg', profile='tiny')
 
 builder = Builder(dwg)
+for index,row in dfs[1].iterrows():
+    print(row['Year'],row['Quarter'],row['Iteration'],row['Start'],row['End'])
+
 for index,row in dfs[0].iterrows():
     x_off = 20
     y_off = 20
     print(row['Type'],row['Start'],row['Summary'])
+    background = None
     if (row['Type'] == 'Intervention'):
-        builder.add(Bonbon(x_off,(y_off+25*index),150,row['Summary']))
-    elif (row['Type'] == 'Impediment'):
-        builder.add(Bonbon(x_off,(y_off+25*index),150,row['Summary'],background='orange'))
+        background = 'blue'
+    if (row['Type'] == 'Impediment'):
+        background='orange'
+    builder.add(Bonbon(x_off,(y_off+25*index),150,row['Summary'],background=background))
 builder.draw()
 
 
