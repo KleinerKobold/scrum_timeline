4ee1b4798b6690ca5d26e17b9e58da9ce9622f37
Oliver Staudt <Trollabeissa@gmail.com>
2021-09-29 16:40:36 +0200

    flake 8 repairs

--- scrum_timeline.py
+++ scrum_timeline.py
@@ -9,33 +9,43 @@ from elements.bonbon import Bonbon
 from elements.builder import Builder
 from util import getBackground
 
+
 def main():
     dfs = read_excel('events.xlsx')
 
     dwg = svgwrite.Drawing('svgwrite-example.svg', profile='tiny')
 
     builder = Builder(dwg)
-    for index,row in dfs[1].iterrows():
-        print(row['Year'],row['Quarter'],row['Iteration'],row['Start'],row['End'])
-        sprint = Sprint(200*index,0,200,row['Year'],row['Quarter'],row['Iteration'],row['Start'],row['End'])
+    for index, row in dfs[1].iterrows():
+        print(row['Year'], row['Quarter'], row['Iteration'],
+              row['Start'], row['End'])
+        sprint = Sprint(200*index, 0, 200,
+                        row['Year'], row['Quarter'], row['Iteration'],
+                        row['Start'], row['End'])
         builder.add(sprint)
 
-    for index,row in dfs[0].iterrows():
+    for index, row in dfs[0].iterrows():
         x_off = 20
         y_off = 50
-        print(row['Type'],row['Start'],row['Summary'])
+        print(row['Type'], row['Start'], row['Summary'])
+        list_contents = list()
         for sprint in Sprint.list_sprints:
             start = row['Start']
             end = row['End']
             contains = sprint.contains(start, start if pd.isna(end) else end)
-            logger.debug(f"item {row['Summary']} at {start:%Y-%m-%d} {'is' if contains else 'is not'} {sprint}")
-        builder.add(Bonbon(x_off,(y_off+25*index),150,row['Summary'],background=getBackground(row['Type'])))
+            if contains:
+                list_contents.append(sprint.getContentPoint())
+            logger.debug(f"item {row['Summary']} at {start:%Y-%m-%d} \
+                         {'is' if contains else 'is not'} {sprint}")
+        builder.add(
+            Bonbon(x_off, (y_off+25*index), 150,
+                   row['Summary'], background=getBackground(row['Type'])))
     builder.draw()
-
 
     print(dwg.tostring())
     dwg.save()
     logger.debug("Done")
+
 
 if __name__ == '__main__':
     main()
