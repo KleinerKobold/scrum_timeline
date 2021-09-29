4b2a306810123bfcc87b74fdf2a6f95f414b6c93
oliver staudt <trollabeissa@gmail.com>
2021-09-29 13:53:44 +0200

    main()

--- scrum_timeline.py
+++ scrum_timeline.py
@@ -7,24 +7,28 @@ from elements.bonbon import Bonbon
 from elements.builder import Builder
 from util import getBackground
 
-dfs = read_excel('events.xlsx')
+def main():
+    dfs = read_excel('events.xlsx')
 
-dwg = svgwrite.Drawing('svgwrite-example.svg', profile='tiny')
+    dwg = svgwrite.Drawing('svgwrite-example.svg', profile='tiny')
 
-builder = Builder(dwg)
-for index,row in dfs[1].iterrows():
-    print(row['Year'],row['Quarter'],row['Iteration'],row['Start'],row['End'])
-    sprint = Sprint(200*index,0,200,row['Year'],row['Quarter'],row['Iteration'],row['Start'],row['End'])
-    builder.add(sprint)
+    builder = Builder(dwg)
+    for index,row in dfs[1].iterrows():
+        print(row['Year'],row['Quarter'],row['Iteration'],row['Start'],row['End'])
+        sprint = Sprint(200*index,0,200,row['Year'],row['Quarter'],row['Iteration'],row['Start'],row['End'])
+        builder.add(sprint)
 
-for index,row in dfs[0].iterrows():
-    x_off = 20
-    y_off = 50
-    print(row['Type'],row['Start'],row['Summary'])
-    builder.add(Bonbon(x_off,(y_off+25*index),150,row['Summary'],background=getBackground(row['Type'])))
-builder.draw()
+    for index,row in dfs[0].iterrows():
+        x_off = 20
+        y_off = 50
+        print(row['Type'],row['Start'],row['Summary'])
+        builder.add(Bonbon(x_off,(y_off+25*index),150,row['Summary'],background=getBackground(row['Type'])))
+    builder.draw()
 
 
-print(dwg.tostring())
-dwg.save()
-logger.debug("Done")
+    print(dwg.tostring())
+    dwg.save()
+    logger.debug("Done")
+
+if __name__ == '__main__':
+    main()
