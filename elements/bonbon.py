d0839347df8ef1c1f0166a41dae939171ccc657f
Oliver Staudt <Trollabeissa@gmail.com>
2021-09-29 16:50:39 +0200

    flake 8

--- elements/bonbon.py
+++ elements/bonbon.py
@@ -1,8 +1,9 @@
 from elements.element import Element
-import svgwrite
+
 
 class Bonbon(Element):
-    def __init__(self,x : int, y:int, width:int, title:str, background='blue', foreground='white') -> None:
+    def __init__(self, x: int, y: int, width: int, 
+                 title: str, background='blue', foreground='white') -> None:
         super().__init__()
         self.x = x
         self.y = y
@@ -12,10 +13,15 @@ class Bonbon(Element):
         self.foreground = foreground
 
     def draw(self, dwg):
-        r= 10
-        dwg.add(dwg.circle((self.x,self.y),(r),fill=self.background))
-        dwg.add(dwg.circle((self.x+self.width,self.y),(r),fill=self.background))
-        dwg.add(dwg.rect((self.x, self.y-r), (self.width,r*2),fill=self.background))
-        dwg.add(dwg.text(text=self.title,insert=(self.x,self.y+3),
-            fill=self.foreground, font_size='12px', font_family="Arial"))
+        r = 10
+        dwg.add(dwg.circle((self.x, self.y),
+                           (r), fill=self.background))
+        dwg.add(dwg.circle((self.x+self.width, self.y),
+                           (r), fill=self.background))
+        dwg.add(dwg.rect((self.x, self.y-r),
+                         (self.width, r*2), fill=self.background))
+        dwg.add(dwg.text(text=self.title,
+                         insert=(self.x, self.y+3),
+                         fill=self.foreground,
+                         font_size='12px', font_family="Arial"))
     
