c3ab0e1398cc5061b36e5eb62835d59a62d96b5b
Oliver Staudt <Trollabeissa@gmail.com>
2021-10-03 21:54:12 +0200

    flake 8

--- read_excel.py
+++ read_excel.py
@@ -1,8 +1,10 @@
 import pandas
+
+
 def read_excel(file_name: str):
     try:
         sprint_data_df = pandas.read_excel('events.xlsx', sheet_name='Sprints')
         data_df = pandas.read_excel('events.xlsx',sheet_name='Data')
-        return (data_df,sprint_data_df)
+        return (data_df, sprint_data_df)
     except ValueError:
         raise("Unable to load excel file")
